fced3529d5849b5433fd323c7b52727bd209eb99
Jan Henning <jh+vsc-hg@buttercookie.de>
2023-12-12 20:07:01 +0100

    Don't include appcompat until we really need it

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,7 +31,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.annotation:annotation:1.7.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
