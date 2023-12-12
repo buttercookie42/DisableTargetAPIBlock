cf973b342652f829bc2386cab5ca09878fc6db7c
Jan Henning <jh+vsc-hg@buttercookie.de>
2023-12-12 22:45:00 +0100

    Hook PackageInstallHelper to always bypass the low target SDK block

--- app/src/main/java/de/buttercookie/disabletargetapiblock/DisableTargetApiBlock.java
+++ app/src/main/java/de/buttercookie/disabletargetapiblock/DisableTargetApiBlock.java
@@ -1,17 +1,49 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 package de.buttercookie.disabletargetapiblock;
+
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+import static de.robv.android.xposed.XposedHelpers.getIntField;
+import static de.robv.android.xposed.XposedHelpers.getObjectField;
+import static de.robv.android.xposed.XposedHelpers.setIntField;
 
 import androidx.annotation.Keep;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class DisableTargetApiBlock implements IXposedHookLoadPackage {
+    private static final String LOGTAG = "DisableTargetApiBlock";
+
+    private static final int INSTALL_BYPASS_LOW_TARGET_SDK_BLOCK = 0x01000000;
+
     @Keep
     public DisableTargetApiBlock() {}
 
     @Keep
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        if (!lpparam.packageName.equals("android")) {
+            return;
+        }
 
+        findAndHookMethod("com.android.server.pm.InstallPackageHelper", lpparam.classLoader,
+                "preparePackageLI", "com.android.server.pm.InstallRequest", new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                        Object request = param.args[0];
+                        Object installArgs = getObjectField(request, "mInstallArgs");
+                        if (installArgs == null) {
+                            return;
+                        }
+
+                        int installFlags = getIntField(installArgs, "mInstallFlags");
+                        installFlags |= INSTALL_BYPASS_LOW_TARGET_SDK_BLOCK;
+                        setIntField(installArgs, "mInstallFlags", installFlags);
+                    }
+                });
     }
 }
