46babf7349df4aedd0a200e7b27070fed35aad43
Jan Henning <jh+vsc-hg@buttercookie.de>
2023-12-12 20:04:34 +0100

    Initial project settings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,14 +11,15 @@ android {
         minSdk = 34
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
