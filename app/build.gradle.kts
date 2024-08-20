6a9db842e5a0cb2644bfa2d62183672b6dade4a2
Jan Henning <jh+vsc-hg@buttercookie.de>
2024-08-20 21:32:34 +0200

    Bump version to 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "de.buttercookie.disabletargetapiblock"
         minSdk = 34
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0.0"
+        versionCode = 3
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
