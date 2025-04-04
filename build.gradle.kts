756e64de27550353f80c00cd8d364206861322d1
Jan Henning <jh+vsc-hg@buttercookie.de>
2025-04-04 23:31:09 +0200

    Upgrade AGP and Gradle versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.2.1" apply false
+    id("com.android.application") version "8.9.1" apply false
     id("com.sidneysimmons.gradle-plugin-external-properties") version "2.0.1" apply false
 }
